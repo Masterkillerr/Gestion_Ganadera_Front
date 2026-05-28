42525aa536c66804ec32c39037ff5e1a6ccb2968
Masterkillerr <alvaropro.0605@gmail.com>
2026-05-27 23:31:39 -0500

    Revert "Add Cloudflare Workers configuration"

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   base: './',
-  plugins: [tailwindcss(), react(), cloudflare()],
+  plugins: [tailwindcss(), react()],
   server: {
     port: 5173,
   },
